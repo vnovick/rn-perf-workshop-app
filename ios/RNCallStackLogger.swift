8d1868af1a77b1353d01c697b98fa0a0f53d12c5
Vladimir Novick <vladimirnovick@Vladimirs-MacBook-Pro.local>
2023-11-07 23:48:03 +0200

    update perf workshop app

---
+++ ios/RNCallStackLogger.swift
@@ -0,0 +1,8 @@
+//
+//  RNCallStackLogger.swift
+//  rnperformanceworkshop
+//
+//  Created by Vladimir Novick on 07/11/2023.
+//
+
+import Foundation
---
+++ ios/RNCallstackLogger.m
@@ -0,0 +1,8 @@
+//
+//  RNCallstackLogger.m
+//  rnperformanceworkshop
+//
+//  Created by Vladimir Novick on 07/11/2023.
+//
+
+#import <Foundation/Foundation.h>
